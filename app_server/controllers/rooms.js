ab14708812e4a516197148c05356d060bfc810f5
NoahColeman89 <colemannoahs@gmail.com>
2023-05-21 21:17:29 -0600

    Express website, static HTML only

--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,6 +1,10 @@
+var fs = require('fs');
+
+var accoms = JSON.parse(fs.readFileSync('./data/accoms.json', 'utf8'));
+
 /* GET rooms view */
 const rooms = (req, res) => {
-    res.render('rooms', { title: 'Travlr Getaways' });
+    res.render('rooms', { title: 'Travlr Getaways', accoms });
 };
 
 module.exports = {
